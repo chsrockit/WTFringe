1ccd8a7e42f30a083dcf0268359702b5f1e67aec
Simon <svansintjan@gmail.com>
2011-05-06 21:17:32 +0100

    lets try again

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php 
-$info = array('see this', 'see that');
-
-
-
+$info = array('GO FUCKING SEE THIS', '', 'see these');
 ?>
+
+<html>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.min.js" > </script>
+
+<body>
+<?php 
+
+$int = array_rand($info);
+echo $info[$int];
+?>
+    
+</body>
+</html>
+
