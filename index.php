f39f809206f2eb18fb39108dd5dd081c5646cdf1
Jonathan Abourbih <jon.abourbih@gmail.com>
2011-05-06 21:32:50 +0100

    Added font-face download

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ $info = array('GO FUCKING SEE THIS', '', 'see these');
 <!doctype html>
 <html>
 <meta charset="utf-8">
+<link href='http://fonts.googleapis.com/css?family=Didact+Gothic' rel='stylesheet' type='text/css'>
 <link type="text/css" rel="stylesheet" href="css/wtf.css">
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.min.js" > </script>
 
